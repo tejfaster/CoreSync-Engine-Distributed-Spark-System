78ff91a6e0b5321cdb0c6c4187fd0d18bbb1cc01
tejfaster <tejfaster55@gmail.com>
2026-03-26 22:33:43 +0100

    Add region-based flight partitioning

--- test_ingestion.py
+++ test_ingestion.py
@@ -1,9 +1,14 @@
 from src.ingestion.flight_api import fetch_flight_data
+from src.processing.region_split import split_by_region
 
 data = fetch_flight_data()
 
+eu, asia = split_by_region(data)
+
 print("Number of Flights:",len(data))
+print("Europe Flights:",len(eu))
+print("Asia Flights:",len(asia))
 
 # numbers of flight
 for flight in data[:3]:
-    print(flight)
+    # print(flight)
